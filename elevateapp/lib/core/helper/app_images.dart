e82751189a33b76bda33ed8cc4db18effe53a6c0
John Doe <johndoe@email.com>
2024-09-24 05:46:33 +0300

    add failure folder ,app constants, app images, spacing, app colors and text styles files

---
+++ elevateapp/lib/core/helper/app_images.dart
@@ -0,0 +1,4 @@
+class  AppImages {
+  static const String splashLogo='assets/storelogo.png';
+  static const String nativeSplashLogo='assets/Frame2.png';
+}
---
+++ elevateapp/lib/core/theming/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color mainBlack = Colors.black;
+  static const Color mainWhite = Colors.white;
+  static const Color mainGrey = Colors.grey;
+  static const Color mainRed = Colors.red;
+  static const Color lightBlue = Colors.blue;
+  static const Color darkBlue = Color.fromARGB(255, 24, 63, 130);
+  static const Color lightGrey = Color.fromARGB(255, 246, 244, 244);
+  static const Color amberColor = Colors.amber;
+}
